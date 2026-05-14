828a51830b5e145798052efc95127c82d1d7f932
Harris-Zintambila <harriszintambila9@gmail.com>
2026-05-14 19:12:23 +0200

    Remove chemistry

--- src/Profile/profile.jsx
+++ src/Profile/profile.jsx
@@ -223,7 +223,7 @@ function Profile() {
                 <option value="Mathematics">Mathematics</option>
                 <option value="Physics">Physics</option>
                 <option value="Biology">Biology</option>
-                <option value="Chemistry">Chemistry</option>
+              
               </select>
             </div>
             <div>
